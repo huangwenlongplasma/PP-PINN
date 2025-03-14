60f86cfc4da6a08538dd8021681c0abed1c084db
huangwenlongplasma <whuang@ahut.edu.cn>
2025-03-14 17:01:42 +0800

    Update num3.py

--- The heat equation/PC-PINN/num3.py
+++ The heat equation/PC-PINN/num3.py
@@ -1,1 +1,285 @@
+import scipy
+import torch
+import torch.optim as optim
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.interpolate import griddata
+import time
 
+
+# 定义PINN模型
+class PINN(torch.nn.Module):
+    def __init__(self):
+        super(PINN, self).__init__()
+        self.layer1 = torch.nn.Linear(2, 70)
+        self.layer2 = torch.nn.Linear(70, 70)
+        self.layer3 = torch.nn.Linear(70, 70)
+        self.layer4 = torch.nn.Linear(70, 70)
+        self.layer5 = torch.nn.Linear(70, 70)
+        self.layer6 = torch.nn.Linear(70, 70)
+        self.layer7 = torch.nn.Linear(70, 70)
+        self.layer8 = torch.nn.Linear(70, 70)
+        self.layer9 = torch.nn.Linear(70, 1)
+
+
+    def forward(self, x, t):
+        inputs = torch.cat([x, t], dim=1)
+        x = torch.sin(self.layer1(inputs))
+        x = torch.sin(self.layer2(x))
+        x = torch.sin(self.layer3(x))
+        x = torch.sin(self.layer4(x))
+        x = torch.sin(self.layer5(x))
+        x = torch.sin(self.layer6(x))
+        x = torch.sin(self.layer7(x))
+        x = torch.sin(self.layer8(x))
+
+
+        return self.layer9(x)
+
+
+# 定义损失函数
+def compute_loss(model, x, t):
+    alpha = 3
+    c = 0.2
+    l = 2
+    u_s = torch.sin(torch.pi * x + 2 * torch.pi * c * t)
+    u_c = torch.cos(torch.pi * x + 2 * torch.pi * c * t)
+    u = model(x, t)
+    u_t = torch.autograd.grad(u.sum(), t, create_graph=True)[0]
+    u_x = torch.autograd.grad(u.sum(), x, create_graph=True)[0]
+    u_xx = torch.autograd.grad(u_x.sum(), x, create_graph=True)[0]
+    f = u_t - c*u**l * u_xx*(alpha**l) - c*(l*u**(l-1)*u_x**l)*(alpha**l) +c*(alpha**l * l * u**(l-1) * (2 * torch.pi * u_c)**2\
+        - alpha**l * u**l * (2 * torch.pi**2 * u_s) - 4 * torch.pi * torch.cos(2 * torch.pi * c*t + torch.pi * x))
+    loss_f = torch.mean(f ** 2)
+
+
+    # Initial condition loss
+    u0 = model(x, torch.zeros_like(t))
+    loss_ic = torch.mean((u0 -(1 + 2 * torch.sin(torch.pi * x))) ** 2)
+
+    # Boundary condition loss
+    u_left = model(torch.zeros_like(x), t)
+    u_right = model(torch.ones_like(x), t)
+    loss_bc1 = torch.mean((u_left - (1 + 2 * torch.sin(2 * torch.pi *c* t))) ** 2)
+    loss_bc2 = torch.mean((u_right - (1 + 2 * torch.sin(2 * torch.pi * c*t + torch.pi))) ** 2)
+    loss_bc = loss_bc1 + loss_bc2
+
+    loss = loss_f + loss_ic + loss_bc
+
+    return loss, loss_f, loss_ic, loss_bc
+
+
+
+# 主函数
+if __name__ == '__main__':
+    torch.manual_seed(1234)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # 创建随机训练数据集
+    num_samples = 5000
+    x_train = (torch.rand(num_samples, 1)).to(device)
+    x_train.requires_grad = True
+    t_train = (5 * torch.rand(num_samples, 1)).to(device)
+    t_train.requires_grad = True
+
+    # Adam优化器
+    model = PINN().to(device)
+    learning_rate = 0.001
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    num_epochs = 8000
+
+    start_time = time.time()
+    # 使用Adam训练模型
+    losses = []
+    pde_losses = []
+    ic_losses = []
+    bc_losses = []
+    for epoch in range(num_epochs):
+        optimizer.zero_grad()
+        loss, pde_loss, ic_loss, bc_loss = compute_loss(model, x_train, t_train)
+        loss.backward()
+        losses.append(loss.item())
+        pde_losses.append(pde_loss.item())
+        ic_losses.append(ic_loss.item())
+        bc_losses.append(bc_loss.item())
+        optimizer.step()
+        if epoch % 100 == 0:
+            print(
+                f'Epoch {epoch}, Loss: {loss.item()}, PDE Loss: {pde_loss.item()}, IC Loss: {ic_loss.item()}, BC Loss: {bc_loss.item()}')
+
+    # LBFGS优化器
+    lbfgs_losses = []
+    lbfgs_loss_f = []
+    lbfgs_loss_ic = []
+    lbfgs_loss_bc = []
+    iteration = 0  # 初始化迭代计数
+
+
+    def closure():
+        global iteration  # 声明iteration为全局变量，这样我们可以在函数内部修改它
+        optimizer_lbfgs.zero_grad()
+        loss, loss_f, loss_ic, loss_bc = compute_loss(model, x_train, t_train)
+        lbfgs_losses.append(loss.item())
+        lbfgs_loss_f.append(loss_f.item())
+        lbfgs_loss_ic.append(loss_ic.item())
+        lbfgs_loss_bc.append(loss_bc.item())
+        print(
+            f"Iteration: {iteration} | Total Loss: {loss.item()}| Loss_f: {loss_f.item()} | Loss_ic: {loss_ic.item()} | Loss_bc: {loss_bc.item()}")
+
+        loss.backward()
+        iteration += 1
+        return loss
+
+
+    optimizer_lbfgs = torch.optim.LBFGS(model.parameters(), max_iter=50000, max_eval=None, tolerance_grad=0,
+                                        tolerance_change=0, history_size=100, line_search_fn=None)
+    optimizer_lbfgs.step(closure)
+    end_time = time.time()
+    print(f"Time elapsed for training: {(end_time - start_time) / 60} min")
+
+    Total_losses = losses + lbfgs_losses
+    PDE_losses = pde_losses + lbfgs_loss_f
+    IC_losses = ic_losses + lbfgs_loss_ic
+    BC_losses = bc_losses + lbfgs_loss_bc
+    plt.figure(figsize=(10, 6))
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.plot(Total_losses, label='Total Loss')
+    plt.plot(PDE_losses, label='PDE Loss')
+    plt.plot(IC_losses, label='IC Loss')
+    plt.plot(BC_losses, label='BC Loss')
+    plt.axvline(x=num_epochs, color='grey', linestyle='--', linewidth=1.5)
+    plt.xlabel('Iteration', fontsize=16)
+    plt.ylabel('Loss', fontsize=16)
+    plt.tick_params(axis='both', labelsize=18)  # 同时设置x轴和y轴的刻度字体大小
+    plt.title('Loss During Training with Adam and L-BFGS', fontsize=23)
+    plt.legend(fontsize=18)
+    plt.savefig('fig/loss_pinnnon_0.2.png')
+    plt.show()
+    # 测试模型
+    model.eval()
+    x = torch.linspace(0, 1, 1000)
+    t = torch.linspace(0, 1, 1000)
+    X, T = torch.meshgrid(x, t)
+    X = X.reshape(-1, 1)
+    T = T.reshape(-1, 1)
+    X = X.to(device, dtype=torch.float32)
+    T = T.to(device, dtype=torch.float32)
+    t1 = torch.linspace(0, 0.2, 1000)
+    X1, T1 = torch.meshgrid(x, t1)
+    X1 = X1.reshape(-1, 1)
+    T1 = T1.reshape(-1, 1)
+    X1 = X1.to(device, dtype=torch.float32)
+    T1 = T1.to(device, dtype=torch.float32)    
+    real_u = 1 + 2 * torch.sin(torch.pi*(2 * T1 + X1))
+    uxt = model(X, T)
+    real_u = 3*real_u.cpu().detach().numpy()
+    print(real_u.shape)
+    print(X.shape)
+    print(T.shape)
+    uxt = 3*uxt.cpu().detach().numpy()
+    L2_error1 = np.linalg.norm((real_u - uxt).flatten(), 2) / np.linalg.norm(real_u.flatten(), 2)
+    print('相对误差: %e' % (L2_error1))
+    Error1 = np.mean(np.abs(real_u - uxt))
+    print('绝对误差: %e' % (Error1))
+    
+    x = torch.linspace(0, 1, 1000)
+    t = torch.linspace(0, 5, 1000)
+    X, T = torch.meshgrid(x, t)
+    X = X.reshape(-1, 1)
+    T = T.reshape(-1, 1)
+    X = X.to(device, dtype=torch.float32)
+    T = T.to(device, dtype=torch.float32)
+    t1 = torch.linspace(0, 1, 1000)
+    X1, T1 = torch.meshgrid(x, t1)
+    X1 = X1.reshape(-1, 1)
+    T1 = T1.reshape(-1, 1)
+    X1 = X1.to(device, dtype=torch.float32)
+    T1 = T1.to(device, dtype=torch.float32)    
+    real_u = 1 + 2 * torch.sin(torch.pi*(2 * T1 + X1))
+    uxt = model(X, T)
+    real_u = 3*real_u.cpu().detach().numpy()
+    print(real_u.shape)
+    print(X.shape)
+    print(T.shape)
+    uxt = 3*uxt.cpu().detach().numpy()
+    L2_error = np.linalg.norm((real_u - uxt).flatten(), 2) / np.linalg.norm(real_u.flatten(), 2)
+    print('相对误差: %e' % (L2_error))
+    Error = np.mean(np.abs(real_u - uxt))
+    print('绝对误差: %e' % (Error))
+    relative_error = (real_u - uxt) / real_u
+    relative_error = relative_error.reshape(1000, 1000)
+    
+#     plt.figure(figsize=(10, 6))
+#     cp = plt.contourf(T.cpu().detach().numpy().reshape(1000, 1000), X.cpu().detach().numpy().reshape(1000, 1000), np.abs(relative_error), 50, cmap='jet')
+#     plt.colorbar(cp)  # 显示颜色条
+#     plt.title('Relative Error')
+#     plt.xlabel('t')
+#     plt.ylabel('x')
+#     plt.savefig('fig/ptnon_error_1_1.png')
+#     plt.show()
+
+#     # 保存数据
+#     x = torch.linspace(0, 1, 1000).to(device)
+#     t = torch.linspace(0, 0.2, 1000).to(device)
+#     x = x.unsqueeze(1)
+#     t = t.unsqueeze(1)
+#     solution = model(x, t)
+#     print(solution.shape)
+#     scipy.io.savemat('data/ptnon_pred5_1.mat',
+#                      {'x': x.cpu().detach().numpy().reshape(-1, 1), 't': t.cpu().detach().numpy().reshape(-1, 1),
+#                       'u': solution.cpu().detach().numpy().reshape(-1, 1)})
+#     t_fp = torch.ones_like(x) * 1
+#     u_fp = model(x, t_fp)
+#     scipy.io.savemat('data/non_fp5_1.mat',
+#                      {'x': x.cpu().detach().numpy().reshape(-1, 1), 't': t_fp.cpu().detach().numpy().reshape(-1, 1),
+#                       'u': u_fp.cpu().detach().numpy().reshape(-1, 1)})
+
+    fig = plt.figure(figsize=(10, 6))
+    uxt_flat = uxt.flatten()
+    T_scaled = (T - T.min()) / (T.max() - T.min())
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_zticks([uxt_flat.min(), (uxt_flat.max() + uxt_flat.min()) / 2, uxt_flat.max()]) # z 轴分为三个刻度
+    z_ticks = [uxt_flat.min(), (uxt_flat.max() + uxt_flat.min()) / 2, uxt_flat.max()]
+    ax.set_zticklabels([f'{z:.1f}' for z in z_ticks])
+    surf = ax.plot_surface(X.cpu().detach().numpy().reshape(1000, 1000), T_scaled.cpu().detach().numpy().reshape(1000, 1000),uxt.reshape(1000, 1000), cmap='jet')
+    # fig.colorbar(surf, shrink=0.5, aspect=5)
+    ax.set_title('PC-PINN', fontsize=23)
+    ax.set_xlabel('x', fontsize=23)
+    ax.set_ylabel('t', fontsize=23)
+#     ax.set_zlabel('u', fontsize=16)
+    ax.tick_params(axis='x', labelsize=18)
+    ax.tick_params(axis='y', labelsize=18)
+    ax.tick_params(axis='z', labelsize=18)
+    ax.xaxis.labelpad = 20
+    ax.yaxis.labelpad = 20
+    ax.zaxis.labelpad = 20
+    ax.set_yticks([0.0, 0.5, 1.0])
+    ax.set_xticks([0.0, 0.5, 1.0])
+    plt.savefig('fig/ptnon_pred_5_pinn+p.png')
+#     plt.savefig('fig/lcr_real_1_3d.pdf')
+    plt.show()
+
+    fig = plt.figure(figsize=(10, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    surf2 = ax.plot_surface(X.cpu().detach().numpy().reshape(1000, 1000), T_scaled.cpu().detach().numpy().reshape(1000, 1000),real_u.reshape(1000, 1000), cmap='jet')
+    # fig.colorbar(surf, shrink=0.5, aspect=5)
+    
+    ax.set_zticks([min(real_u), (max(real_u) + min(real_u)) / 2, max(real_u)])  # z 轴分为三个刻度
+    ax.set_title('Exact Solution', fontsize=23)
+    
+    ax.set_ylabel('t', fontsize=23)
+    ax.set_yticks([0.0, 0.5, 1.0])
+    ax.set_xlabel('x', fontsize=23)
+    ax.set_xticks([0.0, 0.5, 1.0])
+#     ax.set_zlabel('u', fontsize=16)
+    ax.tick_params(axis='x', labelsize=18)
+    ax.tick_params(axis='y', labelsize=18)
+    ax.tick_params(axis='z', labelsize=18)
+    ax.xaxis.labelpad = 20
+    ax.yaxis.labelpad = 20
+    ax.zaxis.labelpad = 20
+    plt.savefig('fig/non_real_0.1_3_2.png')
+#     plt.savefig('fig/non_real_1_2.pdf')
+
+    plt.show()
